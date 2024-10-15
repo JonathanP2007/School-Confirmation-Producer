416394bc10694b29d6acb720169872dd3cde7e79
JonathanP2007 <150850374+JonathanP2007@users.noreply.github.com>
2024-10-15 21:47:20 +0300

    Update School-Confirmation-Producer.py

--- School-Confirmation-Producer.py
+++ School-Confirmation-Producer.py
@@ -1,3 +1,8 @@
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "Pillow"])
+
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
